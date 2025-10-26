c73a0017028f4c83bac34a9c5dd2bc2d0f98f9d5
Morgan Chu <mtychu@kamakura.home>
2025-10-26 01:46:27 -0400

    move code to reusable functions and add input checking

--- anki_utils/anki_connect.py
+++ anki_utils/anki_connect.py
@@ -41,3 +41,41 @@ def invoke(action, **params):
         return None
 
     return result.get("result")
+
+
+# Function to find all decks
+def find_decks():
+    decks = invoke("deckNames")
+    return decks or []
+
+
+# Function to find card IDs in a specific deck
+def card_ids(deck_name):
+    # Validate deck_name is in decks
+    decks = find_decks()
+
+    if deck_name not in decks:
+        raise ValueError(f"Deck '{deck_name}' does not exist. Available decks: {decks}")
+
+    return invoke("findCards", query=f'deck:"{deck_name}"')
+
+
+# Function to get info for a specific card ID and Deck
+def card_info(card_id, deck_name=None):
+    # Validate card_id is an integer
+    try:
+        card_id = int(card_id)
+    except ValueError:
+        raise ValueError("Card ID must be an integer.")
+
+    card_info = invoke("cardsInfo", cards=[card_id])
+    if not card_info:
+        return None
+
+    card_info = card_info[0]  # Get the first (and only) card info
+
+    # If deck_name is provided, validate the card exiists in that deck
+    if deck_name and card_info.get("deckName") != deck_name:
+        return None
+
+    return card_info
--- scripts/card_merge.py
+++ scripts/card_merge.py
@@ -2,18 +2,12 @@ from anki_utils.anki_connect import invoke
 
 # Run using '-m' flag (i.e. python -m scripts.card_merge)
 
-# Print Decks
-decks = invoke("deckNames")
-if decks:
-    print("Decks in Anki:")
-    for deck in decks:
-        print("-", deck)
 
-# find card IDs in a specific deck
-card_ids = invoke("findCards", query='deck:"Core 10k"')
-if not card_ids:
-    print("no cards bro")
-else:
-    print(len(card_ids))
-    card_info = invoke("cardsInfo", cards=[card_ids[0]])
-    print(card_info)
+# Print data for a specific card
+def card_info(card_id, deck_name=None):
+    try:
+        card_id = int(card_id)
+    except ValueError:
+        raise ValueError("Card ID must be an integer.")
+
+    card_info = invoke("cardsInfo", cards=[card_id])
